51acd7cbe118bffc9972fd0db8e42d5fce5509f5
alonecandies <dragonfrezze@gmail.com>
2022-03-16 01:22:23 +0700

    completed

--- src/api/getTopMovies.ts
+++ src/api/getTopMovies.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const TopMovieIds: string[] = ['tt0111161', 'tt0068646', 'tt0071562', 'tt0468569', 'tt0050083'];
 
-const topMoviesInfo = TopMovieIds.map((id) => axios.get(`http://www.omdbapi.com/?i=${id}&apikey=837aeecc`));
+const topMoviesInfo = TopMovieIds.map((id) => axios.get(`http://www.omdbapi.com/?i=${id}&apikey=837aeecc`),);
 
 export default topMoviesInfo;
